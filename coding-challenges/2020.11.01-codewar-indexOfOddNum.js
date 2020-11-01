5810005b338b2858ae9044cbbe39a8b0b7a050b3
Ellie Nguyen <ellienguyendev@gmail.com>
2020-11-01 09:21:41 -0800

    11.01 codewar find index of odd num

---
+++ coding-challenges/2020.11.01-codewar-indexOfOddNum.js
@@ -0,0 +1,8 @@
+// Create a function that takes an array of numbers, and returns the index of the first odd number.
+// oddNum([2, 4, 6, 7, 10]) // => 3
+// oddNum([12, 11, 24, 78, 13, 86, 28]) // => 1
+// If there are no odd numbers, the function should output -1. Numbers are guaranteed to be non-negative.
+
+function oddNum(arr) {
+  return arr.findIndex(e => e %2 != 0)
+}
