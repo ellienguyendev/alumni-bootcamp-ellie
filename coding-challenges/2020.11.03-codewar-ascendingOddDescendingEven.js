76d251a16e922199ed7222aaf4b2f7b87a1e686f
Ellie Nguyen <ellienguyendev@gmail.com>
2020-11-03 07:29:14 -0700

    11.03 codewar ascending odd descending even

---
+++ coding-challenges/2020.11.03-codewar-ascendingOddDescendingEven.js
@@ -0,0 +1,17 @@
+// You are given an array of integers. Your task is to sort odd numbers within the array in ascending order, and even numbers in descending order.
+//
+// Note that zero is an even number. If you have an empty array, you need to return it.
+//
+// For example:
+//
+// [5, 3, 2, 8, 1, 4]  -->  [1, 3, 8, 4, 5, 2]
+//
+// odd numbers ascending:   [1, 3,       5   ]
+// even numbers descending: [      8, 4,    2]
+
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+  const even = array.filter((x) => x % 2 == 0).sort((a,b) => b - a);
+
+  return array.map((x) => x % 2 ? odd.shift() : even.shift())
+}
