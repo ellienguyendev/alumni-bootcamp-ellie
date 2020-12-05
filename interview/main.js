6d777f56acbe835aaab4d125ce65cac02a96cc31
Ellie Nguyen <ellienguyendev@gmail.com>
2020-12-04 20:00:00 -0600

    practice codewars

---
+++ interview/main.js
@@ -0,0 +1,13 @@
+//gicen arr of ints as strings and numbers
+// return sum as if all number 
+// arr with mixed int and string 
+// return sum of elements 
+
+//[4, '1','2'] => 7 
+// ['2',3,'5'] => 10
+
+const sumIntStrings = arr => {
+    return arr.reduce((acc,i) => acc + Number(i),0)
+}
+
+console.log(sumIntStrings(['2',3,'5']))
